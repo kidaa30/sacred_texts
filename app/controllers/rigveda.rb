7c6f6a431affb7e46e2ca46406d59ca7d9330789
Alan Kydd <akydd@ualberta.net>
2013-06-07 23:33:58 -0600

    Working with Rigveda now.  Basic lookup added, but no tests.

--- app/controllers/rigveda.rb
+++ app/controllers/rigveda.rb
@@ -5,17 +5,17 @@ class App < Sinatra::Base
   end
 
   # GET rigveda/mandalas/{mandala}/suktas/{sukta}/rcas/{rc}
-  get %r{/api/v1/rigveda/mandalas/([\d]+)/suktas/([\d]+)/rcas/([\d]+)(\.[\w]+)?} do |mandala, sukta, rc, type|
-
+  get %r{/api/v1/rigveda/mandalas/([\d]+)/suktas/([\d]+)/rcas/([\d]+)} do |mandala, sukta, rc|
+    content_type :json
     result = Rigveda.find_by_mandala_and_sukta_and_rc(mandala.to_i,
                                                       sukta.to_i,
                                                       rc.to_i)
 
     if result.nil?
       status 404
-      format({"error" => "No results found."}, type)
+      {"error" => "No results found."}.to_json
     else
-      format(result, type)
+      result.to_json
     end
   end
 
@@ -39,74 +39,4 @@ class App < Sinatra::Base
     data.to_json
   end
 
-  # Base url for complete search, passage lookup
-  get '/api/v1/rigveda/search' do
-    passage = params['passage']
-    type = params['type']
-
-    # cannot be both a passage and a search
-    if (!passage.nil? && !@search.nil?)
-      status 400
-      data = 
-        {
-          "error" => "Only one of the parameters 'passage' and 'search' can be specified."
-        }
-    elsif !@search.nil?
-      result = Rigveda.by_keyword_search(nil, nil, @search, @mode, @num, @page)
-      data =
-        {
-          "results" => result.to_a,
-          "total_count" => result.count
-        }
-      add_paging!(data)
-    elsif !passage.nil?
-      data = {"passage" => "todo"}
-    else
-      data =
-        {
-          "error" => "This resource is only available for searching via the search url parameter."
-        }
-    end
-    format(data, type)
-  end
-
-  # rigveda search, per sukta
-  get %r{/api/v1/rigveda/mandalas/([\d]+)/suktas/([\d]+)/search} do |mandala, sukta|
-    content_type :json
-
-    if !@search.nil?
-      result = Rigveda.by_keyword_search(mandala, sukta, @search, @mode, @num, @page)
-      data =
-        {
-          "results" => result.to_a,
-          "total_count" => result.count
-        }
-      add_paging!(data)
-      data.to_json
-    else
-      {
-        "error" => "This resource is only available for searching via the search url parameter."
-      }.to_json
-    end
-  end
-
-  # rigveda search, per mandala
-  get %r{/api/v1/rigveda/mandala/([\d]+)/search} do |mandala|
-    content_type :json
-
-    if !@search.nil?
-      result = Bible.by_keyword_search(mandala, nil, @search, @mode, @num, @page)
-      data =
-        {
-          "results" => result.to_a,
-          "total_count" => result.count
-        }
-      add_paging!(data)
-      data.to_json
-    else
-      {
-        "error" => "This resource is only available for searching via the search url parameter."
-      }.to_json
-    end
-  end
 end
--- app/models/rigveda.rb
+++ app/models/rigveda.rb
@@ -5,10 +5,28 @@ class Rigveda
 
   set_collection_name 'Rigveda'
 
-  key :mandala, String
+  key :mandala, Integer
   key :sukta, Integer
   key :rc, Integer
   key :text, String
+
+  def self.by_keyword_search(mandala, sukta, keywords, mode, limit, page)
+    clause = KeywordParser.keyword_where_clause(keywords, mode)
+    if (!mandala.nil?)
+      clause.push({:mandala => mandala.to_i})
+    end
+    if (!sukta.nil?)
+      clause.push({:sukta => sukta.to_i})
+    end
+    # This does not allow me to retrieve the total result count in a single
+    # query
+    #
+    # where(:$and => clause).paginate({
+    #   :per_page => limit,
+    #   :page => page,
+    # })
+    where(:$and => clause).limit(limit).skip(limit * (page - 1))
+  end
 
   def serializable_hash(options = {})
     super({:except => :id}.merge(options))
